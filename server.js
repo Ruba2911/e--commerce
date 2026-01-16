fc79811cb0282bd7e09e2ee900104fb52535d6b6
Ruba2911 <rubadevi2911@gmail.com>
2026-01-16 09:30:48 +0530

    Fix static files path for production

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
